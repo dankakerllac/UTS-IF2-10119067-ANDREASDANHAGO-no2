b5fee94d15d6b146249b93ba7551b9a9887250f9
User <User@DESKTOP-6N6EHH6>
2020-12-04 21:21:01 +0700

    PBOIF2-10119067

---
+++ src/uts/if2/pkg10119067/andreasdanhago/no2/uts.java
@@ -0,0 +1,35 @@
+  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119067.andreasdanhago.no2;
+import java.util.Scanner;
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Tabungan
+ */
+public class uts.if2.pkg10119067.andreasdanhago.no2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int saldoAwal,jumlahUang;
+        
+        System.out.println("====Program Penarikan Uang====");
+        System.out.print("Masukkan Saldo Awal : ");
+        saldoAwal = scanner.nextInt();
+        Tabungan tabungan = new Tabungan(saldoAwal);
+        System.out.print("Jumlah uang yang diambil : ");
+        jumlahUang = scanner.nextInt();
+        System.out.println("Saldo anda sekarang : " + tabungan.ambilUang(jumlahUang));        
+    }  
+}
